6056bea50c1b817c2f4468945d715b80250e2066
zuttorepo <walletzuttocoin@gmail.com>
2025-07-07 18:25:21 +0700

    Update storage.js
    Signed-off-by: zuttorepo <walletzuttocoin@gmail.com>

--- js/storage.js
+++ js/storage.js
@@ -1,10 +1,9 @@
 const Storage = (() => {
   const getWallets = () => JSON.parse(localStorage.getItem("wallets") || "[]");
-  const saveWallets = (data) => localStorage.setItem("wallets", JSON.stringify(data));
+  const saveWallets = (wallets) => localStorage.setItem("wallets", JSON.stringify(wallets));
+  const setCurrentIndex = (index) => localStorage.setItem("currentIndex", index);
   const getCurrentIndex = () => parseInt(localStorage.getItem("currentIndex") || "0");
-  const setCurrentIndex = (i) => localStorage.setItem("currentIndex", i);
 
-  const getWallet = () => getWallets()[getCurrentIndex()];
   const saveWallet = (wallet) => {
     const wallets = getWallets();
     wallets.push(wallet);
@@ -15,29 +14,8 @@ const Storage = (() => {
   return {
     getWallets,
     saveWallets,
-    getCurrentIndex,
     setCurrentIndex,
-    getWallet,
-    saveWallet,
+    getCurrentIndex,
+    saveWallet
   };
 })();
-const Storage = {
-  getWallets: () => {
-    return JSON.parse(localStorage.getItem("wallets") || "[]");
-  },
-  saveWallets: (wallets) => {
-    localStorage.setItem("wallets", JSON.stringify(wallets));
-  },
-  setCurrentIndex: (index) => {
-    localStorage.setItem("currentIndex", index);
-  },
-  getCurrentIndex: () => {
-    return parseInt(localStorage.getItem("currentIndex") || "0");
-  },
-  saveWallet: (wallet) => {
-    const wallets = Storage.getWallets();
-    wallets.push(wallet);
-    Storage.saveWallets(wallets);
-    Storage.setCurrentIndex(wallets.length - 1);
-  }
-};
