ed8c486e0a0324cb64d295db1e148e3edbcb6cf8
zuttorepo <walletzuttocoin@gmail.com>
2025-07-09 17:20:23 +0700

    Update balance.php
    Signed-off-by: zuttorepo <walletzuttocoin@gmail.com>

--- api/balance.php
+++ api/balance.php
@@ -1,20 +1,19 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') exit(0);
 header('Content-Type: application/json');
-$address = $_GET['address'];
 
-$conn = new mysqli("sql107.infinityfree.com", "if0_39159903", "ZuttoCoinWallet", "if0_39159903_XXX");
+$conn = new mysqli("sql107.infinityfree.com", "if0_39159903", "ZuttoCoinWallet", "if0_39159903_zuttocoinrpc");
 
-if ($conn->connect_error) {
-  echo json_encode(["error" => "DB error"]); exit;
-}
+$address = $conn->real_escape_string($_GET['address']);
+$res = $conn->query("SELECT balance FROM wallets WHERE address = '$address'");
 
-$stmt = $conn->prepare("SELECT balance FROM wallets WHERE address = ?");
-$stmt->bind_param("s", $address);
-$stmt->execute();
-$result = $stmt->get_result();
-
-if ($row = $result->fetch_assoc()) {
-  echo json_encode(["balance" => $row["balance"]]);
+if ($row = $res->fetch_assoc()) {
+  echo json_encode(["balance" => $row['balance']]);
 } else {
   echo json_encode(["balance" => 0]);
 }
+
+$conn->close();
